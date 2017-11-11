6241f4ecafabb9f87affe6097ab4c0f5cf8f32e6
sys4 <sys4@free.fr>
2017-11-11 17:37:21 +0100

    Change mobs api and add new mobs from (water_mobs, pmobs, kpgmobs, mob_horse)

---
+++ mods/mobs_monster/init.lua
@@ -0,0 +1,38 @@
+
+local path = minetest.get_modpath("mobs_monster")
+
+-- Intllib
+local S
+if minetest.global_exists("intllib") then
+	if intllib.make_gettext_pair then
+		-- New method using gettext.
+		S = intllib.make_gettext_pair()
+	else
+		-- Old method using text files.
+		S = intllib.Getter()
+	end
+else
+	S = function(s) return s end
+end
+mobs.intllib = S
+
+-- Monsters
+
+dofile(path .. "/dirt_monster.lua") -- PilzAdam
+dofile(path .. "/dungeon_master.lua")
+dofile(path .. "/oerkki.lua")
+dofile(path .. "/sand_monster.lua")
+dofile(path .. "/stone_monster.lua")
+dofile(path .. "/tree_monster.lua")
+dofile(path .. "/lava_flan.lua") -- Zeg9
+dofile(path .. "/mese_monster.lua")
+dofile(path .. "/spider.lua") -- AspireMint
+dofile(path .. "/minotaur.lua") -- NALC(sys4 fork MFF) Kalabasa
+
+-- Bosses
+dofile(path .. "/pumpkins.lua") -- NALC(sys4 fork MFF)
+dofile(path .. "/mese_dragon.lua") -- NALC(sys4 fork MFF) farfadet46
+
+dofile(path .. "/lucky_block.lua")
+
+print ("[MOD] Mobs Redo 'Monsters' loaded")
---
+++ mods/mobs_monster/minotaur.lua
@@ -0,0 +1,100 @@
+
+-- Minotaur Monster by ???
+
+mobs:register_mob("mobs_monster:minotaur", {
+	-- animal, monster, npc, barbarian
+	type = "monster",
+	-- aggressive, deals 11 damage to player when hit
+	passive = false,
+	attack_type = "dogfight",
+	pathfinding = false,
+	reach = 2,
+	damage = 6,
+	-- health & armor
+	hp_min = 45,
+	hp_max = 55,
+	armor = 90,
+	-- textures and model
+	collisionbox = {-0.9,-0.01,-0.9, 0.9,2.5,0.9},
+	visual = "mesh",
+	mesh = "mobs_minotaur.b3d",
+	textures = {
+		{"mobs_minotaur.png"},
+	},
+	visual_size = {x=1, y=1},
+	blood_texture = "mobs_blood.png",
+	-- sounds
+	makes_footstep_sound = true,
+	-- sounds = {
+		-- random = "mobs_zombie",
+		-- damage = "mobs_zombie_hit",
+		-- attack = "mobs_zombie_attack",
+		-- death = "mobs_zombie_death",
+	-- },
+	-- speed and jump
+	walk_velocity = 1,
+	run_velocity = 3,
+	jump = true,
+	floats = 1,
+	view_range = 16,
+	knock_back = 0.05,	--this is a test
+	-- drops desert_sand and coins when dead
+	drops = {
+		{name = "maptools:gold_coin", chance = 40, min = 1, max = 1,},
+		{name = "mobs_monster:minotaur_eye", chance = 2, min = 1, max = 2,},
+		{name = "mobs_monster:minotaur_horn", chance = 4, min = 1, max = 2,},
+		{name = "mobs_monster:minotaur_fur", chance = 1, min = 1, max = 3,},
+	},
+	water_damage = 0,
+	lava_damage = 5,
+	light_damage = 0,
+	-- model animation
+	animation = {
+		speed_normal = 15,		speed_run = 15,
+		stand_start  = 0,		stand_end = 19,
+		walk_start = 20,		walk_end  = 39,
+		run_start  = 20,		run_end = 39,
+		punch_start = 40,		punch_end = 50,
+	},
+					   })
+mobs:alias_mob("mobs:minotaur", "mobs_monster:minotaur")
+
+-- spawns on desert sand between -1 and 20 light, 1 in 100000 chance, 1 Minotaur in area up to 31000 in height
+mobs:spawn_specific("mobs_monster:minotaur", {"default:dirt_with_dry_grass"}, {"air"}, -1, 20, 30, 100000, 1, -31000, 31000, false)
+-- register spawn egg
+mobs:register_egg("mobs_monster:minotaur", "Minotaur", "mobs_minotaur_inv.png", 1)
+
+minetest.register_craftitem("mobs_monster:minotaur_eye", {
+	description = "Minotaur Eye",
+	inventory_image = "mobs_minotaur_eye.png",
+	groups = {magic = 1},
+							 })
+mobs:alias_mob("mobs:minotaur_eye", "mobs_monster:minotaur_eye")
+
+minetest.register_craftitem("mobs_monster:minotaur_horn", {
+	description = "Minotaur Horn",
+	inventory_image = "mobs_minotaur_horn.png",
+	groups = {magic = 1},
+							  })
+mobs:alias_mob("mobs:minotaur_horn", "mobs_monster:minotaur_horn")
+
+minetest.register_craftitem("mobs_monster:minotaur_fur", {
+	description = "Minotaur Fur",
+	inventory_image = "mobs_minotaur_fur.png",
+	groups = {magic = 1},
+							 })
+mobs:alias_mob("mobs:minotaur_fur", "mobs_monster:minotaur_fur")
+
+minetest.register_craftitem("mobs_monster:minotaur_lots_of_fur", {
+	description = "Lot of Minotaur Fur",
+	inventory_image = "mobs_minotaur_lots_of_fur.png",
+	groups = {magic = 1},
+								 })
+mobs:alias_mob("mobs:minotaur_lots_of_fur", "mobs_monster:minotaur_lots_of_fur")
+
+minetest.register_craft({
+	output = "mobs_monster:minotaur_lots_of_fur",
+	recipe = {{"mobs_monster:minotaur_fur", "mobs_monster:minotaur_fur"},
+		{"mobs_monster:minotaur_fur", "mobs_monster:minotaur_fur"},
+	},
+})
