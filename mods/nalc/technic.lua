8c48f37eb5a177cdad887fd8f2cc4c456a721476
sys4-fr <sys4@free.fr>
2018-01-08 02:29:34 +0100

    Change mapgen of uranium ore between levels -80 and -28000

--- mods/nalc/technic.lua
+++ mods/nalc/technic.lua
@@ -1,33 +1,32 @@
--- make silver, tin, mithril to be grinded
---[[local recipes = {
-	-- Dusts
-	{"default:tin_lump", "technic:tin_dust 2"},
-	{"moreores:silver_lump", "technic:silver_dust 2"},
-	{"moreores:mithril_lump", "technic:mithril_dust 2"},
-}
-
-for _, data in pairs(recipes) do
-	technic.register_grinder_recipe({input = {data[1]}, output = data[2]})
-end
-
--- dusts
-local function register_dust(name, ingot)
-	local lname = string.lower(name)
-	lname = string.gsub(lname, ' ', '_')
-	if ingot then
-		minetest.register_craft(
-			{
-				type = "cooking",
-				recipe = "technic:"..lname.."_dust",
-				output = ingot,
-			})
-		technic.register_grinder_recipe({ input = {ingot}, output = "technic:"..lname.."_dust 1" })
-	end
-end
-
-register_dust("Mithril", "default:mithril_ingot")
-register_dust("Silver", "default:silver_ingot")
-register_dust("Tin", "default:tin_ingot")
---]]
 -- Add superquarry machine
 dofile(minetest.get_modpath(minetest.get_current_modname()).."/technic/superquarry.lua")
+
+-- Add uranium to below levels
+local uranium_params = {offset = 0, scale = 1, spread = {x = 100, y = 100, z = 100}, seed = 420, octaves = 3, persist = 0.7}
+local uranium_threshold = 0.55
+
+minetest.register_ore({
+	ore_type         = "scatter",
+	ore              = "technic:mineral_uranium",
+	wherein          = "default:stone",
+	clust_scarcity   = 7*7*7,
+	clust_num_ores   = 4,
+	clust_size       = 3,
+	y_min       = -1000,
+	y_max       = -301,
+	noise_params     = uranium_params,
+	noise_threshold = uranium_threshold,
+})
+
+minetest.register_ore({
+	ore_type         = "scatter",
+	ore              = "technic:mineral_uranium",
+	wherein          = "default:stone",
+	clust_scarcity   = 6*6*6,
+	clust_num_ores   = 4,
+	clust_size       = 3,
+	y_min       = -28000,
+	y_max       = -1001,
+	noise_params     = uranium_params,
+	noise_threshold = uranium_threshold,
+})
