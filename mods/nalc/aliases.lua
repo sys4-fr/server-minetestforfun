b3b2218d1cf581730fad77e338f4c2b3aae9ef0d
sys4-fr <sys4@free.fr>
2018-02-18 21:58:52 +0100

    Update submodules bonemeal and farming
    Fix unknow node because of moretrees:acacia_sapling_ongen removed from moretrees mod. Maked an alias to default:acacia_sapling to fix it.

--- mods/nalc/aliases.lua
+++ mods/nalc/aliases.lua
@@ -2,3 +2,8 @@
 if minetest.get_modpath("mobs_monster") and minetest.get_modpath("homedecor") then
 	minetest.register_alias_force("homedecor:cobweb_corner", "mobs:cobweb")
 end
+
+-- Add moretrees:acacia_sapling_ongen alias
+if minetest.get_modpath("moretrees") then
+	minetest.register_alias("moretrees:acacia_sapling_ongen", "default:acacia_sapling")
+end
